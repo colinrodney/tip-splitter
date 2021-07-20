a22fe3424a10d65fb74abaf06533225c35c65b58
C.Renauld Rodney <c.renauld.rodney@gmail.com>
2021-07-20 16:28:15 -0500

    starting work on custom tip input field

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ console.log(PERCENTAGE);
 const SPAN = document.querySelectorAll(".display");
 console.log(SPAN[0], SPAN[1]);
 
+//  DOM ELEMENT - Custom tip input field
+let customTipInput = document.createElement("input");
+let customTipValue = customTipInput.value;
+// document.body.appendChild(customTip);
+
 let tip; // NOW HAS GLOBAL SCOPE
 let billAmount; // GLOBAL SCOPE
 
@@ -50,12 +55,16 @@ PERCENTAGE[3].addEventListener("click", function () {
   console.log(tip.toFixed(2), typeof tip);
 });
 
+// FOR NOW THIS IS THE CUSTOM BUTTON...
 PERCENTAGE[4].addEventListener("click", function () {
-  let percentOfTip = Number(PERCENTAGE[4].value);
-  billAmount = Number(INPUT[0].value);
-  // console.log("25%", billAmount, percentOfTip);
-  tip = billAmount * percentOfTip;
-  console.log(tip.toFixed(2), typeof tip);
+  document.body.appendChild(customTipInput);
+
+  console.log(customTipValue);
+  // let percentOfTip = Number(PERCENTAGE[4].value);
+  // billAmount = Number(INPUT[0].value);
+  // // console.log("25%", billAmount, percentOfTip);
+  // tip = billAmount * percentOfTip;
+  // console.log(tip.toFixed(2), typeof tip);
 });
 
 PERCENTAGE[5].addEventListener("click", function () {
